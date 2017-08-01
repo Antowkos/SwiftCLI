ec53ea26658b2cf718d24add95a78228a0ec6230
Jake Heiser <jakeheiser1@gmail.com>
2017-08-01 13:30:57 -0500

    Update source layout to that expected by Swift 4

--- Package.swift
+++ Package.swift
@@ -1,5 +1,20 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-    name: "SwiftCLI"
+    name: "SwiftCLI",
+    products: [
+        .library(
+            name: "SwiftCLI",
+            targets: ["SwiftCLI"]),
+    ],
+    targets: [
+        .target(
+            name: "SwiftCLI",
+            dependencies: []),
+        .testTarget(
+            name: "SwiftCLITests",
+            dependencies: ["SwiftCLI"]),
+    ]
 )
